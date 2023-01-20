1c22331e8c71395ac3bca022bb14f74779c16cbf
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 01:50:07 +0200

    feat(cheques): get all cheques

--- server/routes/cheque.routes.js
+++ server/routes/cheque.routes.js
@@ -3,5 +3,9 @@ const router = Router();
 
 import * as controller from "../controllers/cheque.controller.js";
 
+router.route("/")
+    // @route   GET api/cheques/
+    // @access  Private (Level 2)
+    .get(controller.getCheques)
 
 export default router;
