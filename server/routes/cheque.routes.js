968ac1de6789d653aa67e0a0a4c760706062752a
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 01:52:43 +0200

    feat(cheques): add a cheque

--- server/routes/cheque.routes.js
+++ server/routes/cheque.routes.js
@@ -3,9 +3,14 @@ const router = Router();
 
 import * as controller from "../controllers/cheque.controller.js";
 
+import { hasLevel3Access } from "../middleware/auth.middleware.js";
+
 router.route("/")
     // @route   GET api/cheques/
     // @access  Private (Level 2)
     .get(controller.getCheques)
+    // @route   POST api/cheques/
+    // @access  Private (Level 3)
+    .post(hasLevel3Access, controller.createCheque);
 
 export default router;
