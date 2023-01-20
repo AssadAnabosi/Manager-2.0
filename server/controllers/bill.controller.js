50c655117e448aaa03965d46333a157175c2afd2
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 00:14:36 +0200

    feat(bills): get all bills

--- server/controllers/bill.controller.js
+++ server/controllers/bill.controller.js
@@ -1,2 +1,15 @@
-import Bill from '../models/bill.model.js';
+import Bill from '../models/Bill.model.js';
 import ResponseError from "../utils/responseError.js";
+
+// @desc    Get all bills
+export const getBills = async (req, res, next) => {
+    try {
+        const bills = await Bill.find();
+        return res.status(200).json({
+            success: true,
+            data: bills,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
