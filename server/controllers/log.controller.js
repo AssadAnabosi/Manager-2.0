81bda9e89da30f27a0ff0742b636d1da188dfaf3
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 00:52:17 +0200

    feat(logs): get all logs

--- server/controllers/log.controller.js
+++ server/controllers/log.controller.js
@@ -2,3 +2,15 @@ import Log from "../models/Log.model.js";
 import Worker from "../models/User.model.js";
 import ResponseError from '../utils/ResponseError.js';
 
+// @desc    Get all logs
+export const getLogs = async (req, res, next) => {
+    try {
+        const logs = await Log.find();
+        return res.status(200).json({
+            success: true,
+            data: logs,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
