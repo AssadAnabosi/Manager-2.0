6381f1a6a0838403fee8aa7d879c89e47e601f15
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-20 21:32:22 +0200

    model(User): defining the user model and encrypting the password

---
+++ server/models/User.model.js
@@ -0,0 +1,85 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import capitalizeFirstLetter from "../utils/capitalizeFirstLetter.js";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "Please provide a first name"],
+    },
+    lastName: {
+        type: String,
+        required: [true, "Please provide a last name"],
+    },
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an Email"],
+        unique: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email",
+        ],
+    },
+    phoneNumber: {
+        type: String,
+        required: false,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minlength: 8,
+        select: false
+    },
+    accessLevel: {
+        type: String,
+        // "User", "Spectator", "Supervisor", or "Administrator
+        default: "User",
+    },
+});
+// @desc    Create a virtual field "fullName" that combines the first and last name
+UserSchema.virtual("fullName").get(function () {
+    return `${this.firstName} ${this.lastName}`;
+});
+
+
+// @desc    Hash the password and capitalize the first letter of the first and last name before saving the user
+UserSchema.pre("save", async function (next) {
+    // if password is NOT modified, then don't hash the hashed value
+    if (this.isModified("password")) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    if (this.isModified("firstName")) {
+        this.firstName = capitalizeFirstLetter(this.firstName);
+    }
+    if (this.isModified("lastName")) {
+        this.lastName = capitalizeFirstLetter(this.lastName);
+    }
+    return next();
+});
+
+UserSchema.set("toJSON", {
+    // @desc    send the virtual field "fullName" in the response of a find query
+    virtuals: true,
+    // @desc    remove the firstName and lastName fields and don't include the id field
+    transform: function (doc, ret) {
+        delete ret.firstName;
+        delete ret.lastName;
+        delete ret.id;
+    },
+});
+
+
+// @desc    Check if password matches hashed password
+UserSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
