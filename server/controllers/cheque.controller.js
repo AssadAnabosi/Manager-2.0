bcf1eae40ca48f1a898fd50017cf3f828e031901
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 01:52:43 +0200

    feat(cheques): add a cheque

--- server/controllers/cheque.controller.js
+++ server/controllers/cheque.controller.js
@@ -15,3 +15,23 @@ export const getCheques = async (req, res, next) => {
         next(error);
     }
 };
+
+// @desc    Create a Cheque
+export const createCheque = async (req, res, next) => {
+    const { serial, dueDate, value, description, isCancelled } = req.body;
+    if (!serial || !dueDate || !value) {
+        return next(new ResponseError("Please provide a serial, due date and value", 400));
+    }
+    try {
+        const payee = await Payee.findById(req.body.payee);
+        if (!payee)
+            return next(new ResponseError("Payee not found", 404));
+        const cheque = new Cheque({
+            serial, dueDate, value, description, payee, isCancelled
+        });
+        await cheque.save();
+        return res.sendStatus(201);
+    } catch (error) {
+        next(error);
+    }
+};
