ba611b7db6a04fa5cbdd2c806f270167791b2bfb
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 01:22:48 +0200

    feat(payees): get all payees

--- server/controllers/payee.controller.js
+++ server/controllers/payee.controller.js
@@ -1,1 +1,14 @@
 import Payee from "../models/Payee.model.js";
+
+// @desc    Get all payees
+export const getPayees = async (req, res, next) => {
+    try {
+        const payees = await Payee.find();
+        return res.status(200).json({
+            success: true,
+            data: payees,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
