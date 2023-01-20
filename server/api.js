31ea18b06470bf7c3bccf9bf69fcc4021ffd6ed2
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-20 23:03:44 +0200

    init(route): users

--- server/api.js
+++ server/api.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 // @desc    Routes Import
 import authRoutes from "./routes/auth.routes.js";
+import usersRoutes from "./routes/user.routes.js";
+
+// @desc    Middleware Import
+import { isAuth } from "./middleware/auth.middleware.js";
 
 // @desc    Routes
 router.use("/auth", authRoutes);
+router.use("/users", isAuth, usersRoutes);
 
 // @desc    Undefined routes
 router.route("*")
---
+++ server/routes/user.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+
+import * as controller from "../controllers/user.controller.js";
+
+
+
+export default router;
