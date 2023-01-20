2d3bd12a1105507cf45cae9a7db897f858ee0caf
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-20 22:29:31 +0200

    feat(change-password): change-password route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,11 +3,16 @@ const router = Router();
 
 import * as controller from "../controllers/auth.controller.js";
 
+import { isAuth } from "../middleware/auth.middleware.js";
+
 // @desc    Register Route
 router.route("/register")
     .post(controller.register)
 // @desc    Login Route
 router.route("/login")
     .post(controller.login)
+// @desc    Change Password Route  
+router.route("/change-password")
+    .post(isAuth, controller.changePassword)
 
 export default router;
