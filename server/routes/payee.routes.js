9f7af660ca56dc8796155081ec88325d516cd28c
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 01:22:48 +0200

    feat(payees): get all payees

--- server/routes/payee.routes.js
+++ server/routes/payee.routes.js
@@ -3,5 +3,10 @@ const router = Router();
 
 import * as controller from "../controllers/payee.controller.js";
 
+// @desc    Routes
+router.route("/")
+    // @route   GET api/payees/
+    // @access  Private (Level 2)
+    .get(controller.getPayees)
 
 export default router;
