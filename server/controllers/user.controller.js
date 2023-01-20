98e821848dc737da4bd7189ee21ab6f4221438fc
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-20 23:34:12 +0200

    feat(users): get all users

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,2 +1,16 @@
 import User from "../models/User.model.js";
 import ResponseError from "../utils/ResponseError.js";
+
+// @desc    Get all users
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({
+            success: true,
+            data: users,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,6 +3,12 @@ const router = Router();
 
 import * as controller from "../controllers/user.controller.js";
 
+import { hasLevel2Access } from "../middleware/auth.middleware.js";
 
+// @route   GET api/users/
+// @desc    Get all users
+// @access  Private (Level 2)
+router.route("/")
+    .get(hasLevel2Access, controller.getUsers);
 
 export default router;
