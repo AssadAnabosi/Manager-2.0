55bef491f3ba56b12303bb49d25273db8d24c688
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-20 21:44:58 +0200

    feat(register): register a new user

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,5 +3,8 @@ const router = Router();
 
 import * as controller from "../controllers/auth.controller.js";
 
+// @desc    Register Route
+router.route("/register")
+    .post(controller.register)
 
 export default router;
