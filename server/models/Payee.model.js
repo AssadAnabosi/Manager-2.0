1264e296e275366c9b36f16926d54ffcceba9297
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 01:42:49 +0200

    model(cheque): defining the cheque model

--- server/models/Payee.model.js
+++ server/models/Payee.model.js
@@ -18,6 +18,22 @@ const PayeeSchema = new mongoose.Schema({
         type: String,
         required: false
     },
+    cheques: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Cheque",
+        },
+    ]
+});
+
+// @desc   set isDeleted property to true and set the payee's cheques' payee property to null in the associated cheques
+PayeeSchema.post("findOneAndDelete", async function (payee) {
+    if (payee.cheques.length === 0) return;
+    payee.cheques.forEach(async (chequeId) => {
+        const cheque = await Cheque
+            .findByIdAndUpdate(chequeId, { payee: null, isDeleted: true }, { new: true, runValidators: true });
+        await cheque.save();
+    });
 });
 
 const Payee = mongoose.model("Payee", PayeeSchema);
