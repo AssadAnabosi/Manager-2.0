d2447f30436a2a81a725156456262f80dd405475
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-20 21:44:58 +0200

    feat(register): register a new user

--- server/api.js
+++ server/api.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 // @desc    Routes Import
-import authRoutes from "./auth.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 // @desc    Routes
 router.use("/auth", authRoutes);
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,2 +1,22 @@
 import User from "../models/User.model.js";
 import ResponseError from "../utils/responseError.js";
+
+// @desc    Register a new user
+export const register = async (req, res, next) => {
+    const { firstName, lastName, username, email, phoneNumber, password } = req.body;
+    //  @desc   Validate user input
+    if (!firstName || !lastName || !username || !email || !password) {
+        return next(new ResponseError("Please provide first and last names, username, email, and password", 400));
+    }
+    try {
+        await User.create({
+            firstName, lastName, username, email, phoneNumber, password
+        });
+        return res.status(201).json({
+            success: true,
+            message: "User created successfully",
+        });
+    } catch (error) {
+        next(error);
+    }
+}
