cefcf2e221cc57141b26b39eb77beddf4daa5825
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 00:14:36 +0200

    feat(bills): get all bills

--- server/routes/bill.routes.js
+++ server/routes/bill.routes.js
@@ -3,5 +3,9 @@ const router = Router();
 
 import * as controller from "../controllers/bill.controller.js";
 
+router.route("/")
+    // @route   GET api/bills/
+    // @access  Private (Level 2)
+    .get(controller.getBills)
 
 export default router;
