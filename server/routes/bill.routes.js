971c276b94243e35746b33841ed5fc206f2f0401
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 00:19:39 +0200

    feat(bills): add a bill

--- server/routes/bill.routes.js
+++ server/routes/bill.routes.js
@@ -3,9 +3,15 @@ const router = Router();
 
 import * as controller from "../controllers/bill.controller.js";
 
+import { hasLevel3Access } from "../middleware/auth.middleware.js";
+
 router.route("/")
     // @route   GET api/bills/
     // @access  Private (Level 2)
     .get(controller.getBills)
+    // @route   POST api/bills/
+    // @access  Private (Level 3)
+    .post(hasLevel3Access, controller.createBill);
+
 
 export default router;
