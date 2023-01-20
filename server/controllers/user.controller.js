af76abdab52a5fdfd4fcc7bb80734373d6191f83
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-20 23:37:26 +0200

    feat(users): get user by id

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -14,3 +14,19 @@ export const getUsers = async (req, res, next) => {
     }
 };
 
+// @desc    Get a user
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return next(new ResponseError("User not found", 404));
+        }
+        return res.status(200).json({
+            success: true,
+            data: user,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
