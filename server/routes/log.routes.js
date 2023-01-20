140ddcbd4dad398895c67dc6d1749fbd9020a1db
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 00:52:17 +0200

    feat(logs): get all logs

--- server/routes/log.routes.js
+++ server/routes/log.routes.js
@@ -3,5 +3,9 @@ const router = Router();
 
 import * as controller from "../controllers/log.controller.js";
 
+router.route("/")
+    // @route   GET api/logs/
+    // @access  Private (Level 2)
+    .get(controller.getLogs)
 
 export default router;
