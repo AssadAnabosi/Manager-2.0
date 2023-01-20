ac56ff10365487f93584879af9c5242a94768f66
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-21 01:50:07 +0200

    feat(cheques): get all cheques

--- server/controllers/cheque.controller.js
+++ server/controllers/cheque.controller.js
@@ -1,3 +1,17 @@
 import Cheque from "../models/Cheque.model.js";
 import Payee from "../models/Payee.model.js";
 import ResponseError from "../utils/responseError.js";
+
+// @desc    Get all Cheques
+export const getCheques = async (req, res, next) => {
+    try {
+        const cheques = await Cheque.find()
+            .populate("payee", "name");
+        return res.status(200).json({
+            success: true,
+            data: cheques,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
