9b9f5b52968d592d28a8156f756036a0a53c8cc4
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-26 00:52:37 +0200

    feat(logs): quries
    fix(logs-model): OTV isn't a virtual value anymore

--- server/controllers/log.controller.js
+++ server/controllers/log.controller.js
@@ -1,14 +1,37 @@
 import Log from "../models/Log.model.js";
 import Worker from "../models/User.model.js";
 import ResponseError from '../utils/ResponseError.js';
+import ReqQueryHelper from "../utils/ReqQueryHelper.js";
+import * as queryHelper from "../utils/queryHelper.js";
 
 // @desc    Get all logs
 export const getLogs = async (req, res, next) => {
+    const { startDate, endDate, search } = ReqQueryHelper(req.query);
     try {
-        const logs = await Log.find().populate("worker", "firstName lastName");
+        const logs = await Log
+            .aggregate(queryHelper.logsQuery(search, startDate, endDate))
+            .sort({ date: -1 });
+
+        const _id = logs.map(({ _id }) => _id)
+
+        let paymentSums = await Log.aggregate(queryHelper.logsPaymentsSum(_id));
+        if (paymentSums.length < 1)
+            paymentSums = [{ paymentsSum: 0 }];
+
+        let attendanceSums = await Log.aggregate(queryHelper.logsAttendanceSums(_id));
+        if (attendanceSums.length < 1) {
+            attendanceSums = [{ daysCount: 0 }];
+            attendanceSums = [{ OTVSum: 0 }];
+        }
+
         return res.status(200).json({
             success: true,
-            data: logs,
+            data: {
+                logs,
+                paymentsSum: paymentSums[0].paymentsSum,
+                daysCount: attendanceSums[0].daysCount,
+                OTVSum: attendanceSums[0].OTVSum
+            }
         });
     } catch (error) {
         next(error);
@@ -22,7 +45,9 @@ export const createLog = async (req, res, next) => {
         // @desc Log validation
         if (!date || !req.body.worker)
             return next(new ResponseError("Please provide a date and workerId", 400));
-
+        if (!isAbsence && (!startingTime || !finishingTime)) {
+            return next(new ResponseError("Please provide a starting time and finishing time", 400));
+        }
         const worker = await Worker.findById(req.body.worker);
         if (!worker)
             return next(new ResponseError("Worker not found", 404));
@@ -39,7 +64,7 @@ export const createLog = async (req, res, next) => {
 // @desc    Get a log
 export const getLog = async (req, res, next) => {
     try {
-        const log = await Log.findById(req.params.id).populate("worker", "firstName lastName");
+        const log = await Log.aggregate(queryHelper.logQuery(req.params.id));
         if (!log)
             return next(new ResponseError("Log not found", 404));
         return res.status(200).json({
@@ -56,6 +81,9 @@ export const getLog = async (req, res, next) => {
 export const updateLog = async (req, res, next) => {
     if (req.body.worker || req.body.date) {
         return next(new ResponseError("You can't change the date or worker", 400));
+    }
+    if (!isAbsence && (!startingTime || !finishingTime)) {
+        return next(new ResponseError("Please provide a starting time and finishing time", 400));
     }
     try {
         const log = await Log.findByIdAndUpdate(req.params.id, req.body, {
