6393cdeb44b08cf89c92fdf29c6eea34b9d566a4
Assad Anabosi <assad.anabosi@icloud.com>
2023-01-26 23:13:01 +0200

    fix(code-rework)

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,14 +5,13 @@ import * as controller from "../controllers/auth.controller.js";
 
 import { isAuth, hasLevel3Access } from "../middleware/auth.middleware.js";
 
-// @desc    Register Route
-router.route("/register")
-    .post(isAuth, hasLevel3Access, controller.register)
-// @desc    Login Route
-router.route("/login")
-    .post(controller.login)
-// @desc    Retrieve current authenticated user
-router.route("/me")
-    .get(isAuth, controller.getMe)
+//  @routes  api/auth
+
+router.post("/register", isAuth, hasLevel3Access, controller.register);
+
+router.post("/login", controller.login);
+
+// @desc    Retrieve current authenticated user info
+router.get("/me", isAuth, controller.getMe);
 
 export default router;
