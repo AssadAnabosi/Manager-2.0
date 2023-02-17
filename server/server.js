c71e96900e1c27a7802ad9ebc6c01e9ca63efaef
Assad Anabosi <assad.anabosi@icloud.com>
2023-02-17 23:43:39 +0200

    fix(app): separate app from server

--- server/server.js
+++ server/server.js
@@ -3,37 +3,11 @@ dotenv.config({
   path: "./config/config.env",
 });
 
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-
-import connectDB, { createAdmin } from "./config/db.config.js";
-import APIRoutes from "./routes/index.js";
-import errorHandler from "./middleware/error.middleware.js";
-import credentials from "./middleware/allowCredentials.middleware.js";
-import corsOptions from "./config/cors.config.js";
+import connectDB from "./config/db.config.js";
+import app from "./app.js";
 
 // @desc    Connect to DB
-connectDB();
-createAdmin();
-
-const app = express();
-app.use(credentials);
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
-
-// @desc    Health Check route
-app.use("/healthcheck", (req, res) => {
-  return res.sendStatus(200);
-});
-
-// @desc    Serving API routes
-app.use("/api", APIRoutes);
-
-// @desc    Error Handler
-// @warn    Must be the last middleware
-app.use(errorHandler);
+await connectDB();
 
 // @desc    Start Server
 const PORT = process.env.PORT || 5000;
