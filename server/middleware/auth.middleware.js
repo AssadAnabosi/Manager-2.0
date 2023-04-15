279eabc27016bdb34e924ad8719d5397522f08cf
Assad Anabosi <assad.anabosi@icloud.com>
2023-04-15 23:06:56 +0300

    fix(auth): unlogging auth reqs

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.model.js";
-import ResponseError from "../utils/responseError.js";
 import { L1, L2, L3, ADMIN } from "../utils/constants/accessLevels.js";
 import * as statusCode from "../utils/constants/statusCodes.js";
 
@@ -16,12 +15,10 @@ export const isAuth = async (req, res, next) => {
   }
 
   if (!accessToken) {
-    return next(
-      new ResponseError(
-        "Not Authenticated To Access This Route",
-        statusCode.NOT_AUTHENTICATED
-      )
-    );
+    return res.status(statusCode.NOT_AUTHENTICATED).json({
+      success: false,
+      message: "Not Authenticated To Access This Route",
+    });
   }
 
   try {
@@ -29,42 +26,38 @@ export const isAuth = async (req, res, next) => {
     const user = await User.findById(decoded.id).select("-logs");
 
     if (!user) {
-      return next(
-        new ResponseError("User Can Not Be Found", statusCode.NOT_FOUND)
-      );
+      return res.status(statusCode.NOT_FOUND).json({
+        success: false,
+        message: "User Can Not Be Found",
+      });
     }
 
     if (!user.active) {
-      return next(
-        new ResponseError(
-          "Your account has been deactivated",
-          statusCode.NOT_AUTHORIZED
-        )
-      );
+      return res.status(statusCode.NOT_AUTHORIZED).json({
+        success: false,
+        message: "Your account has been deactivated",
+      });
     }
     user.active = undefined;
     req.user = user;
 
     return next();
   } catch (error) {
-    return next(
-      new ResponseError(
-        "Not Authenticated To Access This Route",
-        statusCode.NOT_AUTHENTICATED
-      )
-    );
+    // JWT token has expired
+    return res.status(statusCode.NOT_AUTHENTICATED).json({
+      success: false,
+      message: "Not Authenticated To Access This Route",
+    });
   }
 };
 
 // @desc    required accessLevel is higher than User
 export const hasLevel2Access = async (req, res, next) => {
   if (req.user.accessLevel === L1) {
-    return next(
-      new ResponseError(
-        "Not Authorized To Access This Route",
-        statusCode.NOT_AUTHORIZED
-      )
-    );
+    return res.status(statusCode.NOT_AUTHORIZED).json({
+      success: false,
+      message: "Not Authorized To Access This Route",
+    });
   }
 
   return next();
@@ -74,12 +67,10 @@ export const hasLevel2Access = async (req, res, next) => {
 export const hasLevel3Access = async (req, res, next) => {
   hasLevel2Access(req, res, () => {
     if (req.user.accessLevel === L2) {
-      return next(
-        new ResponseError(
-          "Not Authorized To Access This Route",
-          statusCode.NOT_AUTHORIZED
-        )
-      );
+      return res.status(statusCode.NOT_AUTHORIZED).json({
+        success: false,
+        message: "Not Authorized To Access This Route",
+      });
     }
 
     return next();
@@ -88,12 +79,10 @@ export const hasLevel3Access = async (req, res, next) => {
 // @desc    required accessLevel is Administrator
 export const isAdmin = async (req, res, next) => {
   if (req.user.accessLevel !== ADMIN) {
-    return next(
-      new ResponseError(
-        "Not Authorized To Access This Route",
-        statusCode.NOT_AUTHORIZED
-      )
-    );
+    return res.status(statusCode.NOT_AUTHORIZED).json({
+      success: false,
+      message: "Not Authorized To Access This Route",
+    });
   }
 
   return next();
